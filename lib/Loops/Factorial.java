86a7c5fa9ea07e249f6f6866a57d8bac16f0f10b
Prakhar Yadav <yprakher4@gmail.com>
2022-11-16 21:52:26 +0530

    Factorial program in java

---
+++ lib/Loops/Factorial.java
@@ -0,0 +1,11 @@
+import javax.swing.plaf.FontUIResource;
+
+public class Factorial {
+    public static void main(String[] args) {
+        int num=5,Fact=1 ;
+        for (int i = 1; i < num; i++) {
+            Fact = Fact*i;
+        }
+        System.out.println(Fact);
+    }
+}
