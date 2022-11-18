fca4a3c3fca469db2a3f7d97ecab0bb3e0238666
Prakhar Yadav <yprakher4@gmail.com>
2022-11-18 21:33:12 +0530

    Prigram for Sum of two numbers using functions

---
+++ lib/Functions/SumOf2Numbers.java
@@ -0,0 +1,8 @@
+public class SumOf2Numbers {
+    public static void main(String[] args) {
+        Sum(25, 25);
+    }
+    static void Sum(int num1 ,int num2){
+        System.out.println(num1+num2);
+    }
+}
