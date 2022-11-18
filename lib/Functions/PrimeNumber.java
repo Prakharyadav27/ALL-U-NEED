c80aa568dbb6bcbb573f0787acb8f9e0c25cd033
Prakhar Yadav <yprakher4@gmail.com>
2022-11-18 21:45:24 +0530

    Program for checking prime numbes using function

---
+++ lib/Functions/PrimeNumber.java
@@ -0,0 +1,26 @@
+public class PrimeNumber {
+
+    public static void main(String[] args) {
+        PrimeNum(6);
+    }
+  static void PrimeNum(int n){
+      int count = 0;
+      if (n <= 1) {
+        System.out.println("The number is not prime");
+        return;
+      }
+  
+      for (int i = 1; i <= n / 2; i++) {
+        if (n % i == 0) {
+          count++;
+        }
+      }
+      if (count > 1) {
+          System.out.println("The number is not prime");
+      }
+      else {
+        System.out.println("The number is prime");
+      }
+    }
+  }
+ 
