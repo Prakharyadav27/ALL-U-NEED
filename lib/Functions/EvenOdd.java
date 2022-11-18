a60662258e48b4ad7ff8ec4a76a58928c64a87ae
Prakhar Yadav <yprakher4@gmail.com>
2022-11-18 21:29:18 +0530

    Program for even odd function in java

---
+++ lib/Functions/EvenOdd.java
@@ -0,0 +1,12 @@
+public class EvenOdd {
+    public static void main(String[] args) {
+        EvenOdd1(98);
+    }
+    static void EvenOdd1 (int n){
+        if (n%2==0) {
+            System.out.println("EVEN ");
+        } else {
+            System.out.println("ODD");
+        }
+    }
+}
