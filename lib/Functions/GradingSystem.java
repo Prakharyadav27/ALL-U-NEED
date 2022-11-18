50f9f430420d9d00eaa52cc6d6e4c6825fb15b55
Prakhar Yadav <yprakher4@gmail.com>
2022-11-18 21:54:23 +0530

    Program for Gradind system in java

---
+++ lib/Functions/GradingSystem.java
@@ -0,0 +1,27 @@
+public class GradingSystem {
+    public static void main(String[] args) {
+    Grades(86);        
+    }
+
+   /* Marks        Grade 
+    91-100         AA 
+    81-90          AB 
+    71-80          BB 
+    61-70          BC 
+    51-60          CD 
+    41-50          DD 
+    <=40          Fail */
+
+    static void Grades(int score){
+        if (score>=91&&score<=100) {
+            System.out.println("AA");
+        }else if (score>=81&&score<=90) {
+            System.out.println("AB");
+        }else if(score>=71&&score<=80){
+            System.out.println("BB");
+        }else if (score>=61&&score<=70) {
+            System.out.println();
+        }
+        
+    }
+}
